f7f9a737d982486d1a25f8fd35d1b13f766d6171
Andreas <angohz@ymail.com>
2017-07-04 15:42:29 +0200

    Initial commit

---
+++ data_io.py
@@ -0,0 +1,99 @@
+#!usr/bin/python
+
+import random
+import re
+
+def get_inverse_sequence(sequence, alphabet={"A":"T", "C":"G", "G":"C", "T":"A"}):
+	n = len(sequence)
+	inv_sequence = [""]*n
+	for char_position in range(len(sequence)):
+		current_char = sequence[char_position]
+		if current_char in alphabet:
+			inv_sequence[n-char_position-1] = alphabet[current_char]
+		else:
+			print (sequence)
+			print (current_char)
+			print ("Error! Incorrect Alphabet!")
+			break
+	return ''.join(inv_sequence)
+
+def genereate_dna(length=100, alphabet=["A","C","G","T"]):
+	print ("Generating dna sequence ...")
+	dna = []
+	for i in range(length):
+		dna.append(random.choice(alphabet))
+	return dna
+
+def genereate_reads(dna, coverage=50, avg_read_length=50, mutation_pct=0.1, mutation_alphabet=["A","C","G","T"], remove_pct=5, paired_end=False, both_directions=False, verbose=False):
+	print ("Generating reads ...")
+	#print ("mutation_pct = "+str(mutation_pct) + ", remove_pct = "+str(remove_pct))
+	dna_length = len(dna)
+	reads = []
+	alignment = []
+	for i in range(coverage):
+		if verbose:
+			print ("iteration: " + str(i))
+		cur_index = 0
+		while cur_index < dna_length:
+			#next_index = min(cur_index+avg_read_length+random.choice(range(-10,10)), dna_length)
+			next_index = min(cur_index+avg_read_length+int(random.gauss(mu=0, sigma=(1+avg_read_length/5))), dna_length)
+			new_read = dna[cur_index:next_index]
+			for s_i in range(len(new_read)):
+				if random.choice(range(10000)) < mutation_pct*100:
+					if verbose:
+						print ("mutation at read "+str(s_i))
+					new_read[s_i] = random.choice(mutation_alphabet)
+			if random.choice(range(100)) > remove_pct:
+				if both_directions and random.choice(range(100)) > 50:
+					reads.append(get_inverse_sequence(new_read))
+				else:
+					reads.append(''.join(new_read))
+				alignment.append([cur_index, next_index-1])
+			cur_index = next_index
+
+	### TO DO: generate paired-end information
+
+	return reads, alignment
+
+def get_reads_from_file(filename="samplereads.txt"):
+	with open(filename) as fh:
+		reads = fh.readlines()
+	for read_index in range(len(reads)):
+		reads[read_index] = re.sub(r"\s", '', reads[read_index])
+	return reads
+	
+def write_asqg_file(kmers, contig_seqs, edges, k, filename="asqg_file"):
+	print ("Writing asqg-file ...")
+	headline = "HT\t\n"
+	asqg_vertices = ""
+	vertex_count = 0
+	for kmer in kmers:
+		asqg_vertices += "VT\tk_"+str(kmer)+"\t"+contig_seqs[kmer]+"\n"
+		vertex_count += 1
+	asqg_edges = ""
+	for source in edges:
+		for target in edges[source]:
+			asqg_edges += "ED\tk_"+str(source)+" k_"+str(target)+" 1 "+str(k-1)+" "+str(k)+" 0 "+str(k-2)+" "+str(k)+" 0 0\n"
+	outputfile = file(filename, 'w')
+	outputfile.write(headline)
+	outputfile.write(asqg_vertices)
+	outputfile.write(asqg_edges)
+
+def print_alignment(dna, alignments):
+	print ("Alignment of reads:")
+	print ('  '+' |  '.join(dna))
+	read_index = 0
+	alignment_position = 0
+	read_align_string = ""
+	while read_index < len(alignments):
+		while alignment_position < alignments[read_index][0]:
+			read_align_string += "   . "
+			alignment_position += 1
+		while alignment_position <= alignments[read_index][1]:
+			read_align_string += "{:4d} ".format(read_index)
+			alignment_position += 1
+		read_index += 1
+		if alignment_position >= len(dna):
+			print read_align_string
+			read_align_string = ""
+			alignment_position = 0
---
+++ debruijn_graph_builder.py
@@ -0,0 +1,437 @@
+#!usr/bin/python
+
+import random
+import data_io as dio
+
+class Read:
+	def __init__(self, r_id, sequence):
+		self.id = r_id
+		self.sequence = sequence
+		self.length = len(sequence)
+		self.kmers = []
+		self.paired_end_partner = -1
+		
+	def add_paired_end_partner(self, partner_id):
+		self.paired_end_partner = partner_id
+		
+	def add_kmer(self, kmer_id):
+		self.kmers.append(kmer_id)
+
+class Kmer:
+	def __init__(self, k_id, inv_id , sequence, evidence_reads):
+		self.id = k_id
+		self.sequence = sequence
+		self.id_of_inverse_kmer = inv_id
+		self.evidence_reads = evidence_reads
+		
+	def add_evidence(self, evidence_read_id):
+		self.evidence_reads.append(evidence_read_id)
+		
+	def get_evidence_weight(self):
+		return len(self.evidence_reads)
+
+class ContigSequence:
+	def __init__(self, seq_id, inv_id, sequence, kmers, is_relevant = True):
+		self.id = seq_id
+		self.id_of_inverse_seq = inv_id
+		self.sequence = sequence
+		self.kmers = kmers
+		# overlaps (aka edges) are stored in dictionaries
+		# self.overlap[other_sequence] = overlap_id
+		self.overlaps_out = {}
+		self.overlaps_in = {}
+		self.is_relevant = is_relevant
+
+	def get_length(self):
+		return len(self.sequence)
+		
+	def check_if_overlap_exists(self, other_sequence_id):
+		if other_sequence_id in self.overlaps_out:
+			return self.overlaps_out[other_sequence_id]
+		elif other_sequence_id in self.overlaps_in:
+			return self.overlaps_in[other_sequence_id]
+		else:
+			return -1
+
+	def print_data(self):
+		print ("Sequence "+str(self.id))
+		print (self.sequence)
+		print (self.kmers)
+		print ("Overlaps:")
+		print (self.overlaps_out)
+		print (self.overlaps_in)
+		print ("id of inverse sequence: "+str(self.id_of_inverse_seq))
+		if self.is_relevant:
+			print ("is relevant")
+		else:
+			print ("is not relevant")
+
+class SequenceOverlap:
+	def __init__(self, ov_id, length, seq_1, seq_2, evidence_reads):
+		self.id = ov_id
+		self.length = length
+		self.contig_sequence_1 = seq_1
+		self.contig_sequence_2 = seq_2
+		self.evidence_reads = evidence_reads
+		self.is_relevant = True
+	
+	def add_evidence(self, read_id):
+		self.evidence_reads.append(read_id)
+		
+	def get_evidence_weight(self):
+		return len(self.evidence_reads)
+
+	def print_data(self):
+		print ("Overlap "+str(self.id))
+		print ("From seq. "+str(self.contig_sequence_1)+" to seq. "+str(self.contig_sequence_2))
+		if self.is_relevant:
+			print ("is relevant")
+		else:
+			print ("is not relevant")
+	
+class GraphData:
+	def __init__(self, reads, k, verbose = False):
+		print ("Initializing graph ...")
+		self.k_value = k
+
+		self.reads = []
+		self.kmers = []
+		self.sequences = []
+		self.overlaps = []
+
+		if verbose:
+			print ("Construct read database")
+		read_id = 0
+		for r in reads:
+			self.reads.append(Read(read_id, r))
+			read_id += 1
+			
+		# init k-mer database:
+		if verbose:
+			print ("Construct k-mers and sequences ...")
+		# get kmer data:
+		self.get_kmerdata_from_reads(verbose)
+		# construct sequences from kmers:
+		for kmer in self.kmers:
+			if verbose:
+				print ("now consider kmer with id " + str(kmer.id) + ": " + kmer.sequence)
+			seq_id = kmer.id
+			seq_inv_id = kmer.id_of_inverse_kmer
+			self.sequences.append(ContigSequence(seq_id, seq_inv_id, kmer.sequence, [kmer.id]))
+
+		if verbose:
+			print ("Construct overlaps ...")			
+		# construct overlaps between adjacent sequences with read-evidence:
+		for read in self.reads:
+			for kmer_index in range(len(read.kmers)-1):
+				source_kmer_id = read.kmers[kmer_index]
+				target_kmer_id = read.kmers[kmer_index+1]
+				self.increment_overlap(source_kmer_id, target_kmer_id, read.id, False)
+
+	def print_all_reads(self):
+		print ("Reads:")
+		for read in debruijn.reads:
+			print (str(read.id) + ": "+read.sequence)
+			kmer_string = "kmers: "
+			for kmer_id in read.kmers:
+				kmer_string += str(kmer_id) + " "
+			print kmer_string
+
+	def print_all_kmers(self):
+		print ("Kmers:")
+		for kmer in debruijn.kmers:
+			kmer_info_string = "("+str(kmer.id) + ") " + kmer.sequence + ": "
+			for read in kmer.evidence_reads:
+				kmer_info_string += str(read) + " "
+			print kmer_info_string
+
+	def print_all_sequences(self):
+		print ("Sequences:")
+		for s in debruijn.sequences:
+			if s.is_relevant:
+				print ("("+str(s.id)+") "+s.sequence) + ": "
+				s.print_data()
+				for kmer_id in s.kmers:
+					kmer_string = debruijn.kmers[kmer_id].sequence + "\t"
+					for read_id in debruijn.kmers[kmer_id].evidence_reads:
+						kmer_string += str(read_id)+" "
+					print kmer_string
+				print("")
+
+	def get_kmerdata_from_reads(self, verbose = False):
+		print ("Get kmer-data from reads ...")
+
+		read_index = 0
+		kmer_counter = 0
+		for read_index in range(len(self.reads)):
+			if verbose:
+				print ("Current read: "+str(read_index)+" - "+self.reads[read_index].sequence)
+			current_read_sequence = self.reads[read_index].sequence
+			kmer_start = 0
+			while kmer_start <= (len(current_read_sequence) - self.k_value):
+				if verbose:
+					print ("Current kmer-start: "+str(kmer_start))
+				# construct new kmer from read sequence:
+				new_kmer_sequence = current_read_sequence[kmer_start:kmer_start + self.k_value]
+				kmer_already_existing = False
+				this_kmer_id = -1
+				# check if kmer already exists in database:
+				for k_index in range(len(self.kmers)):
+					if verbose:
+						print ("Kmer "+str(k_index)+": "+self.kmers[k_index].sequence)
+						print self.kmers[k_index].evidence_reads
+				k_index = 0
+				#for k_index in range(len(self.kmers)):
+				while k_index < len(self.kmers):
+					if self.kmers[k_index].sequence == new_kmer_sequence:
+						kmer_already_existing = True
+						#if not read_index in self.kmers[k_index].evidence_reads:
+						self.kmers[k_index].add_evidence(read_index)
+						if verbose:
+							print ("Kmer already exists")
+							print ("Add read ("+str(read_index)+") evidence to kmer "+str(k_index))
+						this_kmer_id = k_index
+						#break
+						k_index = len(self.kmers)
+					k_index += 1
+				if not kmer_already_existing:
+					if verbose:
+						print ("Kmer does not exist in database. Add new kmer ...")
+					
+					# add kmer:
+					self.kmers.append(Kmer(kmer_counter, kmer_counter+1, new_kmer_sequence, [read_index]))
+					this_kmer_id = kmer_counter
+					kmer_counter += 1
+					# add inverse kmer:
+					self.kmers.append(Kmer(kmer_counter, kmer_counter-1, get_inverse_sequence(new_kmer_sequence), []))
+					kmer_counter += 1
+					
+				if verbose:
+					print ("Add kmer "+self.kmers[this_kmer_id].sequence+"("+str(this_kmer_id)+") to read "+str(read_index))
+				# add kmer to read:
+				self.reads[read_index].add_kmer(this_kmer_id)
+				kmer_start += 1
+			read_index += 1
+			
+	def increment_overlap(self, source_seq_id, target_seq_id, read_evidence, verbose = False):
+		if target_seq_id not in self.sequences[source_seq_id].overlaps_out:
+			if verbose:
+				print ("Add overlap of tuple " + str(source_seq_id) + " - " + str(target_seq_id))
+				print ("seq_1: "+self.sequences[source_seq_id].sequence)
+				print ("seq_2: "+self.sequences[target_seq_id].sequence)
+			# add new overlap:
+			ov_id = len(self.overlaps)
+			self.overlaps.append(SequenceOverlap(ov_id, self.k_value-1, source_seq_id, target_seq_id, []))
+			self.sequences[source_seq_id].overlaps_out[target_seq_id] = ov_id
+			self.sequences[target_seq_id].overlaps_in[source_seq_id] = ov_id
+			
+			# add inverse overlap:
+			rev_ov_id = ov_id + 1
+			source_rev_seq_id = self.sequences[target_seq_id].id_of_inverse_seq
+			target_rev_seq_id = self.sequences[source_seq_id].id_of_inverse_seq
+			if verbose:
+				print ("Add inverse overlap: "+str(source_rev_seq_id) + " - " + str(target_rev_seq_id))
+				print ("seq_1: "+self.sequences[source_rev_seq_id].sequence)
+				print ("seq_2: "+self.sequences[target_rev_seq_id].sequence)
+			self.overlaps.append(SequenceOverlap(rev_ov_id, self.k_value-1, source_rev_seq_id, target_rev_seq_id, []))
+			self.sequences[source_rev_seq_id].overlaps_out[target_rev_seq_id] = rev_ov_id
+			self.sequences[target_rev_seq_id].overlaps_in[source_rev_seq_id] = rev_ov_id
+			
+		else:
+			ov_id = self.sequences[source_seq_id].overlaps_out[target_seq_id]
+		self.overlaps[ov_id].add_evidence(read_evidence)
+
+	def contract_unique_overlaps(self, verbose = False):
+		# contract unique overlaps:
+		print ("Contract overlaps ...")
+
+		ov_index = 0
+		while ov_index in range(len(self.overlaps)):
+			if self.overlaps[ov_index].is_relevant:
+				source_id = self.overlaps[ov_index].contig_sequence_1
+				if verbose:
+					print ("consider overlap: ")
+					print (self.overlaps[ov_index].print_data())
+					print ("Source: ")
+					print (self.sequences[source_id].print_data())
+					print ("")
+				if (self.sequences[source_id].is_relevant) and (len(self.sequences[source_id].overlaps_out) == 1):
+					ov = self.overlaps[ov_index]
+					target_id = ov.contig_sequence_2
+					if verbose:
+						print ("contract this overlap")
+						print ("target:")
+						self.sequences[target_id].print_data()
+						print ("")
+					if (len(self.sequences[source_id].overlaps_out) == 1) and (len(self.sequences[target_id].overlaps_in) == 1):
+						# if source node has exactly one outgoing edge
+						# and the target node has exactly one incoming edge, 
+						# then contract edge:
+						if verbose:
+							print ("Contract edge: " + str(source_id) + " - " + str(target_id))
+							print ("sequence 1: " + self.sequences[source_id].sequence)
+							print ("sequence 2: " + self.sequences[target_id].sequence)
+						self.sequences[source_id].sequence += self.sequences[target_id].sequence[self.k_value-1:self.sequences[target_id].get_length()]
+						if verbose:
+							print ("combined sequence: " + self.sequences[source_id].sequence)
+						# update outgoing overlaps:
+						self.sequences[source_id].overlaps_out = self.sequences[target_id].overlaps_out
+						# update list of kmers:
+						for kmer in self.sequences[target_id].kmers:
+							if kmer not in self.sequences[source_id].kmers:
+								self.sequences[source_id].kmers.append(kmer)
+						# remove target sequence and overlap from relevant dataset:
+						self.sequences[target_id].is_relevant = False
+						self.overlaps[ov_index].is_relevant = False
+
+						# move outgoing overlaps from target_seq to source_seq:
+						for ov_target_out in self.sequences[target_id].overlaps_out:
+							self.sequences[source_id].overlaps_out[ov_target_out] = self.sequences[target_id].overlaps_out[ov_target_out]
+							self.overlaps[self.sequences[target_id].overlaps_out[ov_target_out]].contig_sequence_1 = source_id
+
+						# update incoming overlaps for adjacent sequences:
+						for adj_seq_id in self.sequences[source_id].overlaps_out:
+							self.sequences[adj_seq_id].overlaps_in[source_id] = self.sequences[adj_seq_id].overlaps_in[target_id]
+							self.sequences[adj_seq_id].overlaps_in.pop(target_id)
+
+						# contract reverse overlap (as above):
+						source_rev_id = self.sequences[target_id].id_of_inverse_seq
+						target_rev_id = self.sequences[source_id].id_of_inverse_seq
+						if verbose:
+							print ("Contract edge: " + str(source_rev_id) + " - " + str(target_rev_id))
+							print ("sequence 1: " + self.sequences[source_rev_id].sequence)
+							print ("sequence 2: " + self.sequences[target_rev_id].sequence)
+						rev_ov_id = self.sequences[source_rev_id].overlaps_out[target_rev_id]
+						self.sequences[source_rev_id].sequence += self.sequences[target_rev_id].sequence[self.k_value-1:self.sequences[target_rev_id].get_length()]
+						if verbose:
+							print ("combined sequence: " + self.sequences[source_rev_id].sequence)
+						self.sequences[source_rev_id].overlaps_out = self.sequences[target_rev_id].overlaps_out
+						for kmer in self.sequences[target_rev_id].kmers:
+							if kmer not in self.sequences[source_rev_id].kmers:
+								self.sequences[source_rev_id].kmers.append(kmer)
+						self.sequences[target_rev_id].is_relevant = False
+						self.overlaps[rev_ov_id].is_relevant = False
+						for ov_target_out in self.sequences[target_rev_id].overlaps_out:
+							self.sequences[source_rev_id].overlaps_out[ov_target_out] = self.sequences[target_rev_id].overlaps_out[ov_target_out]
+							self.overlaps[self.sequences[target_rev_id].overlaps_out[ov_target_out]].contig_sequence_1 = source_rev_id
+
+						# update incoming overlaps for adjacent sequences:
+						for adj_seq_id in self.sequences[source_rev_id].overlaps_out:
+							self.sequences[adj_seq_id].overlaps_in[source_rev_id] = self.sequences[adj_seq_id].overlaps_in[target_rev_id]
+							self.sequences[adj_seq_id].overlaps_in.pop(target_rev_id)
+
+						# update reverse_ids of sequences:
+						self.sequences[source_id].id_of_inverse_seq = source_rev_id
+						self.sequences[source_rev_id].id_of_inverse_seq = source_id
+			ov_index += 1
+
+	def remove_parallel_sequences(self):
+		print ("Remove parallel sequences ...")
+		checked_sequences = [False for seq in self.sequences]
+		for seq_id in range(len(self.sequences)):
+			seq = self.sequences[seq_id]
+			if not seq.is_relevant:
+				checked_sequences[seq_id] = True
+			elif not checked_sequences[seq_id]:
+				all_adjacent_sequences = set([ov for ov in seq.overlaps_out] + [ov for ov in seq.overlaps_in])
+				if len(all_adjacent_sequences) == 0:
+					# case no adjacent sequences:
+					checked_sequences[self.sequences[seq_id].id_of_inverse_seq] = True
+					if self.sequences[seq_id].is_relevant:
+						self.sequences[self.sequences[seq_id].id_of_inverse_seq].is_relevant = False
+				else:
+					# start bfs
+					bfs_queue = [seq_id]
+					while (len (bfs_queue) > 0):
+						print bfs_queue
+						current_seq_id = bfs_queue.pop(0)
+						current_inv_seq_id = self.sequences[current_seq_id].id_of_inverse_seq
+						# mark this sequence and its inverse as checked:
+						checked_sequences[current_seq_id] = True
+						checked_sequences[current_inv_seq_id] = True
+						if self.sequences[current_seq_id].is_relevant:
+							# inverse sequense is not relevant:
+							self.sequences[current_inv_seq_id].is_relevant = False
+							
+							# remove all overlaps starting from inverse seq:
+							for ov_out in self.sequences[current_inv_seq_id].overlaps_out:
+								self.overlaps[self.sequences[current_inv_seq_id].overlaps_out[ov_out]].is_relevant = False
+							for ov_in in self.sequences[current_inv_seq_id].overlaps_in:
+								self.overlaps[self.sequences[current_inv_seq_id].overlaps_in[ov_in]].is_relevant = False
+
+							# add all adjacent sequences to queue:
+							for adj_seq_id in self.sequences[current_seq_id].overlaps_out:
+								print ("consider adj_seq "+str(adj_seq_id))
+								if (not checked_sequences[adj_seq_id]) and (self.sequences[adj_seq_id].is_relevant):
+									print ("Add to bfs")
+									bfs_queue.append(adj_seq_id)
+							for adj_seq_id in self.sequences[current_seq_id].overlaps_in:
+								print ("consider adj_seq "+str(adj_seq_id))
+								if (not checked_sequences[adj_seq_id]) and (self.sequences[adj_seq_id].is_relevant):
+									print ("Add to bfs")
+									bfs_queue.append(adj_seq_id)
+
+	def get_asqg_output(self, filename="asqg_file"):
+		print ("Writing asqg-file ...")
+		headline = "HT\t\n"
+		
+		asqg_vertices = ""
+		vertex_count = 0
+		for seq in self.sequences:
+			if seq.is_relevant:
+				asqg_vertices += "VT\tk_"+str(seq.id)+"\t"+seq.sequence+"\n"
+				vertex_count += 1
+		
+		asqg_edges = ""
+		for ov in self.overlaps:
+			if ov.is_relevant:
+				seq_1_length = self.sequences[ov.contig_sequence_1].get_length()
+				seq_2_length = self.sequences[ov.contig_sequence_2].get_length()
+				asqg_edges += "ED\tk_"+str(ov.contig_sequence_1)+" k_"+str(ov.contig_sequence_2)+" "+str(seq_1_length-self.k_value+1)+" "+str(seq_1_length-1)+" "+str(seq_1_length)+" 0 "+str(self.k_value-2)+" "+str(seq_2_length)+" 0 0\n"
+
+		outputfile = file(filename, 'w')
+		outputfile.write(headline)
+		outputfile.write(asqg_vertices)
+		outputfile.write(asqg_edges)
+
+	def get_csv_output(self, filename="csv_file.csv"):
+		print ("writing csv-file ...")
+		headline = "Node_Label, Sequence\n"
+		data = ""
+		for seq in self.sequences:
+			if seq.is_relevant:
+				data += "k_"+str(seq.id)+","+seq.sequence+"\n"
+		outputfile = file(filename, 'w')
+		outputfile.write(headline)
+		outputfile.write(data)
+		
+def get_inverse_sequence(sequence, alphabet={"A":"T", "C":"G", "G":"C", "T":"A"}):
+	n = len(sequence)
+	inv_sequence = [""]*n
+	for char_position in range(len(sequence)):
+		current_char = sequence[char_position]
+		if current_char in alphabet:
+			inv_sequence[n-char_position-1] = alphabet[current_char]
+		else:
+			print (sequence)
+			print (current_char)
+			print ("Error! Incorrect Alphabet!")
+			break
+	return ''.join(inv_sequence)		
+
+dna = dio.genereate_dna(length=100)
+reads, alignment = dio.genereate_reads(dna, coverage=20, avg_read_length=20, remove_pct=0, mutation_pct=0.2, mutation_alphabet=["A","C","G","T"], both_directions=True)
+
+#reads = [''.join(dna[0:25]), ''.join(dna[15:45]), ''.join(dna[35:65]), ''.join(dna[55:85]), ''.join(dna[75:100])]
+#reads = dio.get_reads_from_file()
+k = 25
+
+#dio.print_alignment(dna, alignment)
+
+debruijn = GraphData(reads, k, verbose = False)
+#debruijn.get_asqg_output(filename = "graph_pre_contract")
+debruijn.contract_unique_overlaps(verbose = False)
+#debruijn.get_asqg_output(filename = "graph_post_contract")
+debruijn.remove_parallel_sequences()
+debruijn.get_asqg_output(filename = "graph_post_unify")
+debruijn.get_csv_output()
