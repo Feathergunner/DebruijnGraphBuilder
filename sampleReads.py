3d038dc62deb67ebe73ed69c868e4b117e78a0ec
Andreas Goral <angohz@ymail.com>
2017-07-06 11:27:51 +0200

    Added funcionality to create reads with different lengths and inverted reads

--- sampleReads.py
+++ sampleReads.py
@@ -1,60 +1,73 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from random import randint
+from random import randint, gauss
 
-with open('bvdv_selected_filtered.linsi.aln') as fh:
-	lines = fh.readlines()
+from data_io import get_inverse_sequence
 
-genomes = []
-sequences = {}
-for line in lines:
-	if (line != ''):
-		if (line[0] == '>'):
-			gname = line.strip()[1:]
-			genomes.append(gname)
-			sequences[gname] = ''
-		else:
-			sequences[gname] += line.strip()
+def samplereads(input_filename="Data/bvdv_selected_filtered.linsi.aln",
+				output_filename="samplereads.txt",
+				read_length=50,
+				length_stddev=0,
+				inverted_reads=False):
 
-
-closegaps = True
-numreads = 10000
-readlen = 50
-reads = []
-
-fractions = [0.5, 0.5, 0, 0, 0]
-
-
-for curgenomeind in range(len(fractions)):
+	# read input genomes:
+	with open(input_filename) as fh:
+		lines = fh.readlines()
 	
-	curgenome = genomes[curgenomeind]
-	curnumreads = int(numreads * fractions[curgenomeind])
+	genomes = []
+	sequences = {}
+	for line in lines:
+		if (line != ''):
+			if (line[0] == '>'):
+				gname = line.strip()[1:]
+				genomes.append(gname)
+				sequences[gname] = ''
+			else:
+				sequences[gname] += line.strip()
 	
-	for n in range(curnumreads):
+	closegaps = True
+	numreads = 10000
+	reads = []
+	
+	fractions = [0.5, 0.5, 0, 0, 0]
+	
+	# construct reads:s
+	for curgenomeind in range(len(fractions)):
 		
-		ind = randint(0, len(sequences[curgenome])-readlen)
-		sampleread = sequences[curgenome][ind:ind+readlen]
-		while ('-' in sampleread):
+		curgenome = genomes[curgenomeind]
+		curnumreads = int(numreads * fractions[curgenomeind])
+		
+		for n in range(curnumreads):
 			
-			if closegaps:
-				nreplace = sampleread.count('-')
-				if (ind + nreplace > len(sequences[curgenome])):
-					ind = randint(0, numreads-readlen)
-					sampleread = sequences[curgenome][ind:ind+readlen]
-				else:
-					added = sequences[curgenome][ind+readlen:ind+readlen+nreplace]
-					if ('-' in added):
+			readlen = read_length + int(gauss(mu=0, sigma=length_stddev))
+			ind = randint(0, len(sequences[curgenome])-readlen)
+			sampleread = sequences[curgenome][ind:ind+readlen]
+			while ('-' in sampleread):
+				
+				if closegaps:
+					nreplace = sampleread.count('-')
+					if (ind + nreplace > len(sequences[curgenome])):
 						ind = randint(0, numreads-readlen)
 						sampleread = sequences[curgenome][ind:ind+readlen]
 					else:
-						sampleread = sampleread.replace('-','') + added
-				
+						added = sequences[curgenome][ind+readlen:ind+readlen+nreplace]
+						if ('-' in added):
+							ind = randint(0, numreads-readlen)
+							sampleread = sequences[curgenome][ind:ind+readlen]
+						else:
+							sampleread = sampleread.replace('-','') + added
+					
+				else:
+					ind = randint(0, numreads-readlen)
+					sampleread = sequences[curgenome][ind:ind+readlen]
+			if (inverted_reads and randint(0, 100)>50):
+				reads.append(get_inverse_sequence(sampleread.upper()))
 			else:
-				ind = randint(0, numreads-readlen)
-				sampleread = sequences[curgenome][ind:ind+readlen]
-		reads.append(sampleread.upper())
-
-with open('samplereads.txt','w') as outf:
-	for read in reads:
-		outf.write(read + '\n')
+				reads.append(sampleread.upper())
+	
+	with open(output_filename, 'w') as outf:
+		for read in reads:
+			outf.write(read + '\n')
+	
+samplereads()
