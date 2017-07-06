f8ea131b3bca0422d7b7621e5f367300e59074c1
Andreas Goral <angohz@ymail.com>
2017-07-06 12:10:29 +0200

    Added function to construct random mutation of nucleotide

--- data_io.py
+++ data_io.py
@@ -16,6 +16,10 @@ def get_inverse_sequence(sequence, alphabet={"A":"T", "C":"G", "G":"C", "T":"A"}
 			print ("Error! Incorrect Alphabet!")
 			break
 	return ''.join(inv_sequence)
+	
+def get_random_mutation(nucleotide, alphabet=['A', 'C', 'G', 'T']):
+	possible_mutations = [c for c in alphabet if not c == nukleotide]
+	return random.choice(possible_mutations)
 
 def genereate_dna(length=100, alphabet=["A","C","G","T"]):
 	print ("Generating dna sequence ...")
