bdde1844eb8fd4b1824b82fb5e28c0d3722ecc46
Andreas Goral <angohz@ymail.com>
2017-07-13 19:19:09 +0200

    Fixed Bug

--- get_bandage_images.py
+++ get_bandage_images.py
@@ -4,9 +4,10 @@ import os
 import re
 import subprocess
 
-path = "Output/errorimpact"
+datapath = "Output/errorimpact"
+bandagepath = "../Bandage"
 
-for datafile in os.listdir(path):
+for datafile in os.listdir(datapath):
 	filenameparts = re.split(r'\.',datafile)
 	if filenameparts[-1] == "asqg":
 		print datafile
@@ -16,7 +17,7 @@ for datafile in os.listdir(path):
 			casename = ".".join(filenameparts[0:-1])
 		print casename
 		
-		command_bandage_get_image = "../../../../code/Bandage/Bandage image ../../uni/Master/code/debruijn_graph_builder/"+path+"/"+datafile+" ../../uni/Master/code/debruijn_graph_builder/"+path+"/"+casename+".png"
+		command_bandage_get_image = bandagepath+"/Bandage image "+datapath+"/"+datafile+" "+datapath+"/"+casename+".png"
 		print command_bandage_get_image
 		subprocess.call(command_bandage_get_image.split())
 		
