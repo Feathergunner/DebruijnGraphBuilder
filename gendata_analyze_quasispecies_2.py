04a39b5dbff929a3b3879d786c5fdda71f189bee
Feathergunner <na87vix@minet.uni-jena.de>
2017-07-16 17:17:12 +0200

    Add experiment script

---
+++ gendata_analyze_quasispecies_2.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import random
+
+import sampleReads
+import data_io as dio
+import fast_debruijn_graph_builder as fdgb
+import manjasDefinitionen as md
+
+sampleReads.read_genomes()
+
+#k_values = [13,15,17,20,25,]
+k_relative_settings = [0.4, 0.5, 0.6, 0.7]
+readlength_settings = [50, 100, 250]
+error_percentage = 0
+
+num_reads_settings = [[50000,50000,10000,5000], [50000,25000,10000,1000], [50000]*10+[5000]*5+[1000]*5]
+viruses = [md.v1, md.v7, md.v3, md.v5, md.v2, md.v4, md.v6, "KP941583.1", "KJ541471.1", "KC757383.1",
+			"KC963967.1", "KF501393.1", "AB078950.1", "KY849592.1", "LT631725.1",
+			"KU159365.1", "KJ620017.1", "LC089875.1", "U86600.1", "KU756226.1"]
+
+for num_reads in num_reads_settings:
+	for readlength in readlength_settings:
+		num_different_viruses = len(num_reads)
+		set_of_viruses = viruses[:num_different_viruses]
+		casename_gen = "bvdv_sample_"+str(readlength)+"_"+str(num_different_viruses)+"_["
+		for n_r_i in range(len(num_reads)-1):
+			casename_gen += str(num_reads[n_r_i])+","
+		casename_gen += str(num_reads[-1])+"]_"+str(error_percentage)
+		readfilename = "Data/reads_"+casename_gen+".txt"
+	
+		sampleReads.samplereads(output_filename			= readfilename,
+								read_length				= readlength,
+								set_of_viruses			= set_of_viruses,
+								number_of_reads			= num_reads,
+								avg_error_percentage	= error_percentage)
+	
+		for k_rel in k_relative_settings:
+			k = int(readlength * k_rel)
+			casename = casename_gen + "_"+str(k)
+		
+			reads = dio.get_reads_from_file(filename = readfilename)
+			debruijn = fdgb.GraphData(reads, k)
+			debruijn.contract_unique_overlaps()
+			debruijn.remove_parallel_sequences()
+		
+			debruijn.get_asqg_output(filename = "Output/"+casename+".asqg")
+			debruijn.get_csv_output(filename = "Output/"+casename+".csv")
