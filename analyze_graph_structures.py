8b0edbfa8038ba1d2284029c88abb4fdbb23e539
Andreas Goral <angohz@ymail.com>
2017-07-27 11:40:11 +0200

    Reorganized code and added plotting of average sequence lengths

--- analyze_graph_structures.py
+++ analyze_graph_structures.py
@@ -8,8 +8,18 @@ import graph_analyzer as ga
 import stochastic as st
 
 sourcedirs_absk = ["general_absk"]#, "general_absk_wot"]
-aourcedirs_relk = ["general_relk"]#, "general_relk_wot"]
+sourcedirs_relk = ["general_relk"]#, "general_relk_wot"]
 
+def multiple_lineplot_nodes_edges_components(graph_analyzer, data, filename):
+	fig = plt.figure()
+	ax1 = fig.add_subplot(111)
+	ax2 = ax1.twinx()
+	fig.set_size_inches(20,20)
+	graph_analyzer.lineplot("num_of_nodes", data, axis=ax1, legend_pos=1)
+	graph_analyzer.lineplot("num_of_edges", data, style='--', axis=ax1, legend_pos=1)
+	graph_analyzer.lineplot("num_of_components", data, style=':', axis=ax2, legend_pos=2)
+	fig.savefig(filename, dpi=80)
+	plt.close()
 
 def plots_absk(sourcedirs):
 	readlength_settings = [50, 100, 250, 500, 1000]
@@ -24,33 +34,18 @@ def plots_absk(sourcedirs):
 				gaga.get_data(case_restrict=gacr, verbose=True)
 				
 				if len(gaga.graphdatas) > 0:
-					fig = plt.figure()
-					ax1 = fig.add_subplot(111)
-					ax2 = ax1.twinx()
-					fig.set_size_inches(20,20)
-					
-					data = "error_percentage"
-					gaga.lineplot("num_of_nodes", data, axis=ax1, legend_pos=1)
-					#plt.gca().set_prop_cycle(None)
-					gaga.lineplot("num_of_edges", data, style='--', axis=ax1, legend_pos=1)
-					#plt.gca().set_prop_cycle(None)
-					gaga.lineplot("num_of_components", data, style=':', axis=ax2, legend_pos=2)
-					fig.savefig("Output/"+sourcedir+"/"+gacr.construct_casename()+"_"+data+".png")
-					plt.close()
-					
-					fig = plt.figure()
-					ax1 = fig.add_subplot(111)
-					ax2 = ax1.twinx()
-					fig.set_size_inches(20,20)
+					multiple_lineplot_nodes_edges_components(gaga, "error_percentage", "Output/"+sourcedir+"/plots/"+gacr.construct_casename()+"_error_percentage.png")
+					multiple_lineplot_nodes_edges_components(gaga, "k_value", "Output/"+sourcedir+"/plots/"+gacr.construct_casename()+"_k_value.png")
 				
-					data = "k_value"
-					gaga.lineplot("num_of_nodes", data, axis=ax1, legend_pos=1)
-					#plt.gca().set_prop_cycle(None)
-					gaga.lineplot("num_of_edges", data, axis=ax1, style='--', legend_pos=1)
-					#plt.gca().set_prop_cycle(None)
-					gaga.lineplot("num_of_components", data, style=':', axis=ax2, legend_pos=2)
-					fig.savefig("Output/"+sourcedir+"/"+gacr.construct_casename()+"_"+data+".png")
+					fig = plt.figure()
+					ax1 = fig.add_subplot(111)
+					ax2 = ax1.twinx()
+					fig.set_size_inches(20,20)
+					data = "error_percentage"
+					gaga.lineplot("avg_seq_lengths", data)
+					fig.savefig("Output/"+sourcedir+"/plots/"+gacr.construct_casename()+"_"+data+"_seqlengths.png")
 					plt.close()
+					
 	
 		for ep in [0, 0.1, 0.25, 0.5, 1, 2, 5]:
 			for i in range(len(coverage_factors)):
@@ -61,22 +56,7 @@ def plots_absk(sourcedirs):
 					gaga.get_data(case_restrict=gacr, verbose=True)
 					
 				if len(gaga.graphdatas) > 0:
-					data = "k_value"
-					#fig, ax1 = plt.subplots()
-					fig = plt.figure()
-					ax1 = fig.add_subplot(111)
-					ax2 = ax1.twinx()
-					fig.set_size_inches(20,20)
-					#plt.figure(figsize=(40,40), dpi=80)
-					
-					gaga.lineplot("num_of_nodes", data, axis=ax1, legend_pos=1)
-					#plt.gca().set_prop_cycle(None)
-					gaga.lineplot("num_of_edges", data, style='--', axis=ax1, legend_pos=1)
-					#plt.gca().set_prop_cycle(None)
-					gaga.lineplot("num_of_components", data, style=':', axis=ax2, legend_pos=2)
-					
-					fig.savefig("Output/"+sourcedir+"/k(-1)_ep("+str(ep)+")_coveragefactor("+str(coverage_factors[i])+")_"+data+".png", dpi=80)
-					plt.close()
+					multiple_lineplot_nodes_edges_components(gaga, "k_value", "Output/"+sourcedir+"/plots/k(-1)_ep("+str(ep)+")_coveragefactor("+str(coverage_factors[i])+")_"+data+".png")
 
 def plots_relk(sourcedirs):
 	k_rel_values = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
@@ -89,33 +69,8 @@ def plots_relk(sourcedirs):
 			gaga.get_data(case_restrict=gacr, verbose=True)
 			
 			if len(gaga.graphdatas) > 0:
-				fig = plt.figure()
-				ax1 = fig.add_subplot(111)
-				ax2 = ax1.twinx()
-				fig.set_size_inches(20,20)
-				
-				data = "error_percentage"
-				gaga.lineplot("num_of_nodes", data, axis=ax1, legend_pos=1)
-				#plt.gca().set_prop_cycle(None)
-				gaga.lineplot("num_of_edges", data, style='--', axis=ax1, legend_pos=1)
-				#plt.gca().set_prop_cycle(None)
-				gaga.lineplot("num_of_components", data, style=':', axis=ax2, legend_pos=2)
-				fig.savefig("Output/"+sourcedir+"/"+gacr.construct_casename()+"_"+data+".png")
-				plt.close()
-				
-				fig = plt.figure()
-				ax1 = fig.add_subplot(111)
-				ax2 = ax1.twinx()
-				fig.set_size_inches(20,20)
-			
-				data = "rel_k_value"
-				gaga.lineplot("num_of_nodes", data, axis=ax1, legend_pos=1)
-				#plt.gca().set_prop_cycle(None)
-				gaga.lineplot("num_of_edges", data, axis=ax1, style='--', legend_pos=1)
-				#plt.gca().set_prop_cycle(None)
-				gaga.lineplot("num_of_components", data, style=':', axis=ax2, legend_pos=2)
-				fig.savefig("Output/"+sourcedir+"/"+gacr.construct_casename()+"_"+data+".png")
-				plt.close()
+				multiple_lineplot_nodes_edges_components(gaga, "error_percentage", "Output/"+sourcedir+"/plots/"+gacr.construct_casename()+"_error_percentage.png")
+				multiple_lineplot_nodes_edges_components(gaga, "rel_k_value", "Output/"+sourcedir+"/plots/"+gacr.construct_casename()+"_rel_k_value.png")
 
 		readlength_settings = [50, 100, 150, 200]
 		number_of_reads_settings = [1000,500,333,250]
@@ -131,19 +86,7 @@ def plots_relk(sourcedirs):
 					gaga.get_data(case_restrict=gacr, verbose=True)
 				
 				if len(gaga.graphdatas) > 0:
-					data = "rel_k_value"
-					fig = plt.figure()
-					ax1 = fig.add_subplot(111)
-					ax2 = ax1.twinx()
-					fig.set_size_inches(20,20)
+					multiple_lineplot_nodes_edges_components(gaga, "rel_k_value", "Output/"+sourcedir+"/plots/k(-1)_ep("+str(ep)+")_coveragefactor("+str(coverage_factor[i])+")_rel_k_value.png")
 				
-					gaga.lineplot("num_of_nodes", data, axis=ax1, legend_pos=1)
-					#plt.gca().set_prop_cycle(None)
-					gaga.lineplot("num_of_edges", data, style='--', axis=ax1, legend_pos=1)
-					#plt.gca().set_prop_cycle(None)
-					gaga.lineplot("num_of_components", data, style=':', axis=ax2, legend_pos=2)
-					
-					fig.savefig("Output/"+sourcedir+"/k(-1)_ep("+str(ep)+")_coveragefactor("+str(coverage_factor[i])+")_"+data+".png", dpi=80)
-					plt.close()
-				
+plots_relk(sourcedirs_relk)
 plots_absk(sourcedirs_absk)
