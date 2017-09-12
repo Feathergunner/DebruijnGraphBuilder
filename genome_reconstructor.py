c2c35d3c47aaa717e56f4a1ce41453b8b4c76de6
Andreas <angohz@ymail.com>
2017-09-12 12:25:05 +0200

    Added first version of partition of sequences

--- genome_reconstructor.py
+++ genome_reconstructor.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import re
+import gc
+
+import fast_debruijn_graph_builder as fdgb
+import data_io as dio
 
 class Node:
 	def __init__(self, id, name, sequence):
@@ -20,6 +24,8 @@ class Graph:
 		self.number_of_edges = 0
 		self.nodes = []
 		self.node_name_dictionary = {}
+		self.max_label = 0
+		self.min_label = 0
 		
 	def add_node(self, node_name, node_seq):
 		self.nodes.append(Node(self.number_of_nodes, node_name, node_seq))
@@ -59,19 +65,67 @@ class Graph:
 			for node_id in self.nodes[current_node_id].adjacencies:
 				if self.nodes[node_id].label == False:
 					start_label = current_data[1] + self.nodes[current_node_id].length
+					if start_label > self.max_label:
+						self.max_label = start_label
 					self.nodes[node_id].label = start_label
 					queue.append([node_id, start_label])
 			for node_id in self.nodes[current_node_id].reverse_adjacencies:
 				if self.nodes[node_id].label == False:
 					start_label = current_data[1] - self.nodes[node_id].length
+					if start_label < self.min_label:
+						self.min_label = start_label
 					self.nodes[node_id].label = start_label
 					queue.append([node_id, start_label])
 					
 	def print_nodes_sorted_by_label(self):
 		sorted_nodes = sorted(self.nodes, key=lambda x: x.label)
 		print [str(n.id)+" ("+n.name+"): "+str(n.label) for n in sorted_nodes]
+
+	def get_partition_of_sequences(self, number_of_parts, overlap):
+		sorted_nodes = sorted(self.nodes, key=lambda x: x.label)
+		label_div = self.max_label-self.min_label
+		
+		parts = []
+		for i in range(number_of_parts):
+			if i == 0:
+				current_start = self.min_label
+			else:
+				current_start = self.min_label+i*(label_div/number_of_parts)-overlap
+			if i == number_of_parts-1:
+				current_end = self.max_label
+			else:
+				current_end = current_start+(i+1)*(label_div/number_of_parts)+overlap
+			print str(i)+": "+str(current_start)+" - "+str(current_end)
+			parts.append([node for node in sorted_nodes if node.label >= current_start and node.label <= current_end])
+		return parts
+
+	def write_node_sequences_to_file(self, filename, nodes):
+		sequence_string = ""
+		for n in nodes:
+			sequence_string += n.sequence + "\n"
+
+		outputfile = file(filename, 'w')
+		outputfile.write(sequence_string)
 		
 g = Graph()
-g.read_graph_from_asqg("Output/general_absk_1/bvdv_sample_250_1_[500]_0-5_16.asqg")
+g.read_graph_from_asqg("Output/corona-largereads-asbk-1/corona-largereads-asbk-1-i_8000_1_[250]_1-0_30.asqg")
 g.construct_assembly_ordering_labels()
-g.print_nodes_sorted_by_label()
+#g.print_nodes_sorted_by_label()
+
+nodesets = g.get_partition_of_sequences(10, 1000)
+i = 0
+for nodeset in nodesets:
+	readfile_name = "reconstruct_sequence_reads_"+str(i)
+	g.write_node_sequences_to_file(readfile_name, nodeset)
+
+	reads = dio.get_reads_from_file(filename = readfile_name)
+	debruijn = fdgb.GraphData(reads, 20)
+	# delete reads and kmers to save ram:
+	debruijn.reads = []
+	debruijn.kmers = []
+	# run garbage collector:
+	gc.collect()
+	debruijn.contract_unique_overlaps(verbose=False)
+	debruijn.remove_parallel_sequences()
+	
+	debruijn.get_asqg_output(filename = "reconstruct_test_"+str(i)+".asqg")
