038dea9a0c6ec927fb47e8b9dc05ff426de1a55c
Andreas Goral <angohz@ymail.com>
2017-09-15 00:35:51 +0200

    Added script to construct debruijn-graph from fastq-reads

---
+++ construct_debruijn_from_coronareads.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import data_io as dio
+import fast_debruijn_graph_builder as fdgb
+
+readfilename = "Data/2017-09-05_coronavirus.fq"
+output_dir = "Output"
+k = 30
+casename = "corona_realreads_k"+str(k)
+
+reads = dio.get_reads_from_fastq_file(readfilename)
+
+debruijn = fdgb.GraphData(reads, k)
+# delete reads and kmers to save ram:
+reads = []
+debruijn.reads = []
+debruijn.kmers = []
+# run garbage collector:
+gc.collect()
+debruijn.contract_unique_overlaps(verbose=False)
+debruijn.remove_parallel_sequences()
+
+debruijn.get_asqg_output(filename = output_dir+"/"+casename+".asqg")
+debruijn.get_csv_output(filename = output_dir+"/"+casename+".csv")
