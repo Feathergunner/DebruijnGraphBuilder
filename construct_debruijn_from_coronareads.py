5fc3952ef2d7cc9ac569ef6cafaacfeb7e167cc1
Andreas Goral <angohz@ymail.com>
2017-09-15 13:58:44 +0200

    Added multiple settings

--- construct_debruijn_from_coronareads.py
+++ construct_debruijn_from_coronareads.py
@@ -9,21 +9,23 @@ import fast_debruijn_graph_builder as fdgb
 readfilename = "Data/2017-09-05_coronavirus.fq"
 output_dir = "Output"
 k = 30
-casename = "corona_realreads_k"+str(k)
-
-reads = dio.get_reads_from_fastq_file(readfilename, 1)
-
-debruijn = fdgb.GraphData([reads], k, verbose=False)
-#print reads
-#print len(reads)
-# delete reads and kmers to save ram:
-reads = []
-debruijn.reads = []
-debruijn.kmers = []
-# run garbage collector:
-gc.collect()
-debruijn.contract_unique_overlaps(verbose=True)
-debruijn.remove_parallel_sequences()
-
-debruijn.get_asqg_output(filename = output_dir+"/"+casename+".asqg")
-debruijn.get_csv_output(filename = output_dir+"/"+casename+".csv")
+for k in [30, 40]:
+	for num_reads in [2000, 5000]:
+		casename = "corona_realreads_n"+str(num_reads)+"_k"+str(k)
+		
+		reads = dio.get_reads_from_fastq_file(readfilename, num_reads)
+		
+		debruijn = fdgb.GraphData([reads], k, alphabet={"A":"U", "U":"A", "G":"C", "C":"G"}, verbose=False)
+		#print reads
+		#print len(reads)
+		# delete reads and kmers to save ram:
+		reads = []
+		debruijn.reads = []
+		debruijn.kmers = []
+		# run garbage collector:
+		gc.collect()
+		debruijn.contract_unique_overlaps(verbose=True)
+		debruijn.remove_parallel_sequences()
+		
+		debruijn.get_asqg_output(filename = output_dir+"/"+casename+".asqg")
+		debruijn.get_csv_output(filename = output_dir+"/"+casename+".csv")
