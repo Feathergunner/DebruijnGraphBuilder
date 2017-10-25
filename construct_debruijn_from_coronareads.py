bd7d20b37b8c6bfe619ea1f6699c664184760ef7
Andreas Goral <angohz@ymail.com>
2017-10-25 14:34:07 +0200

    Modified setting for reconstruction from coronareads

--- construct_debruijn_from_coronareads.py
+++ construct_debruijn_from_coronareads.py
@@ -7,23 +7,28 @@ import data_io as dio
 import fast_debruijn_graph_builder as fdgb
 
 readfilename = "Data/2017-09-05_coronavirus.fq"
-output_dir = "Output"
+output_dir = "Output/corona_realparts"
+if not os.path.exists(output_dir):
+	os.makedirs(output_dir)
 
-for k in [30, 40]:
-	for num_reads in [2000, 5000]:
-		casename = "corona_realreads_n"+str(num_reads)+"_k"+str(k)
-		
-		reads = dio.get_reads_from_fastq_file(readfilename, num_reads)
-		
-		debruijn = fdgb.GraphData([reads], k, alphabet={"A":"U", "U":"A", "G":"C", "C":"G"}, verbose=False)
-		# delete reads and kmers to save ram:
-		reads = []
-		debruijn.reads = []
-		debruijn.kmers = []
-		# run garbage collector:
-		gc.collect()
-		debruijn.contract_unique_overlaps(verbose=False)
-		debruijn.remove_parallel_sequences()
-		
-		debruijn.get_asqg_output(filename = output_dir+"/"+casename+".asqg")
-		debruijn.get_csv_output(filename = output_dir+"/"+casename+".csv")
+k = 40
+num_reads = 2000
+for start in range(1,73000,2000):
+	casename = "corona_realreads_s_"+str(start)+"n_"+str(num_reads)+"_k"+str(k)
+	
+	reads = dio.get_reads_from_fastq_file(readfilename, num_of_reads=num_reads, first_read=start)
+	
+	debruijn = fdgb.GraphData([reads], k, alphabet={"A":"U", "U":"A", "G":"C", "C":"G"}, verbose=False)
+	# delete reads and kmers to save ram:
+	reads = []
+	debruijn.reads = []
+	debruijn.kmers = []
+	# run garbage collector:
+	gc.collect()
+	debruijn.contract_unique_overlaps(verbose=False)
+	debruijn.remove_parallel_sequences()
+	
+	debruijn.remove_single_sequence_components()
+	
+	debruijn.get_asqg_output(filename = output_dir+"/"+casename+".asqg")
+	debruijn.get_csv_output(filename = output_dir+"/"+casename+".csv")
