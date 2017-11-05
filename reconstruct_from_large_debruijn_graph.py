98454c004fa4ad815e825871fa14052952b71c01
Andreas Goral <angohz@ymail.com>
2017-11-05 22:43:39 +0100

    Reorganized code for reconstruction

--- reconstruct_from_large_debruijn_graph.py
+++ reconstruct_from_large_debruijn_graph.py
@@ -8,8 +8,8 @@ import os
 import data_io as dio
 import fast_debruijn_graph_builder as fdgb
 
-def get_sequences_by_params(filename_input, filename_output, min_weight=1, number_of_parts=1, overlap=1):
-	with open(sourcefilename) as inputfile:
+def get_sequences_by_params(filename_input, filename_output, min_weight=1, number_of_parts=1, overlap=1, verbose=False):
+	with open(filename_input) as inputfile:
 		lines = inputfile.readlines()
 	i = 0
 	j = 0
@@ -19,12 +19,16 @@ def get_sequences_by_params(filename_input, filename_output, min_weight=1, numbe
 	
 	filename_output += "_w"+str(min_weight)
 	
+	part_sequence_filenames = []
+	
 	sequences = []
 	for l in lines:
 		if i > 0:
-			if i%10000 == 0:#i>0 and i<100:
+			if i%10000 == 0:
 				print str(i)+"/"+str(n)+" - "+str(j)
 			data = re.split(r',',l)
+			if verbose:
+				print data
 			if int(data[2]) > min_weight:
 				label = int(data[3])
 				sequences.append([data[1], label, data[2]])
@@ -37,11 +41,12 @@ def get_sequences_by_params(filename_input, filename_output, min_weight=1, numbe
 		i+=1
 	
 	if number_of_parts > 1:
-		partition_size = (max_label-min_label)/(number_of_parts-overlap)
-		partition_start_diff = (max_label-min_label)/(number_of_parts+overlap)
+		partition_size = (max_label-min_label)*overlap/number_of_parts
+		partition_start_diff = (max_label-min_label-partition_size)/number_of_parts
 		
 		for i in range(number_of_parts):
 			partname = filename_output+"_p"+str(i)+".txt"
+			part_sequence_filenames.append(partname)
 			part_seqs = []
 			for s in sequences:
 				if s[1] > i*partition_start_diff and s[1] < i*partition_start_diff+partition_size:
@@ -53,19 +58,75 @@ def get_sequences_by_params(filename_input, filename_output, min_weight=1, numbe
 		part_seqs = []
 		for s in sequences:
 			part_seqs.append(s[0]+","+str(s[2])+"\n")
-		outputfile = file(partname, 'w')
+		part_sequence_filenames.append(filename_output+"_p0.txt")
+		outputfile = file(filename_output+"_p0.txt", 'w')
 		for seq in part_seqs:
 			outputfile.write(seq)
+			
+	return part_sequence_filenames
+	
+def reconstruct_part(reads, filename_output, k, minweight=1, minlengthfactor=1, allow_recursion=False):
+	debruijn = fdgb.GraphData(reads, k, verbose=False)
+	# delete reads and kmers to save ram:
+	#reads = []
+	debruijn.reads = []
+	debruijn.kmers = []
+	# run garbage collector:
+	gc.collect()
+	debruijn.contract_unique_overlaps(verbose=False)
+	debruijn.remove_parallel_sequences()
+	
+	#debruijn.reduce_to_single_largest_component()
+	
+	debruijn.remove_insignificant_sequences(minimal_weight=minweight)
+	debruijn.contract_unique_overlaps(verbose = False)
+	debruijn.remove_short_sequences(length_bound_by_multiple_of_k=minlengthfactor)
+	debruijn.contract_unique_overlaps(verbose = False)
 		
-def reconstruct_parts(filename_input_base, filename_output_base, number_of_parts, k1, minweight, minlengthfactor):
+	#debruijn.remove_single_sequence_components()
+	debruijn.construct_assembly_ordering_labels()
+		
+	#debruijn.reduce_to_single_path_max_weight()
+	#debruijn.contract_unique_overlaps(verbose = False)
+		
+	debruijn.get_asqg_output(filename = filename_output+".asqg")
+	debruijn.get_csv_output(filename = filename_output+".csv")
+		
+	debruijn.write_sequences_to_file(filename = filename_output+"_seqsonly.txt", addweights=True)
+	
+	return debruijn.get_label_span()
+		
+def reconstruct_parts(list_of_inputfiles, filename_output_base, k, minweight, minlengthfactor, allow_recursion=False):
+	files_of_parts = []
+
+	partcounter = 0
+	totalparts = len(list_of_inputfiles)
+	for filename in list_of_inputfiles:
+		reads = dio.get_reads_from_file(filename)
+		if len(reads)>0:
+			filename_output = filename_output_base+"_k"+str(k)+"_p"+str(partcounter)
+			partcounter += 1
+			graph_span = reconstruct_part(reads, filename_output, k, minweight, minlengthfactor, allow_recursion)
+			if (allow_recursion):# and graph_span > 100*k):
+				part_sequence_filenames = get_sequences_by_params(filename_input=filename_output+".csv", filename_output=filename_output+"_w"+str(minweight), min_weight=minweight, number_of_parts=100, overlap=10, verbose=True)
+				part_parts = reconstruct_parts_lv2(part_sequence_filenames, filename_output_base=filename_output+"_w"+str(minweight), k=15)
+				
+				merged_parts_seqfilename = reconstruct_merge(filename_output_base=filename_output_base+"_w"+str(minweight)+"_k"+str(17), files_to_merge=part_parts, merge_k=11)
+				filename_output = merged_parts_seqfilename
+			files_of_parts.append(filename_output+"_seqsonly.txt")
+			
+	return files_of_parts
+	
+def reconstruct_parts_lv2(list_of_inputfiles, filename_output_base, number_of_parts, k):
+	files_of_parts = []
 	for i in range(number_of_parts):
 		filename_input = filename_input_base+"_p"+str(i)+".txt"
-		filename_output = filename_output_base+"_k"+str(k1)+"_p"+str(i)
+		filename_output = filename_output_base+"_k"+str(k)+"_p"+str(i)
 		
 		reads = dio.get_reads_from_file(filename_input)
 		if len(reads)>0:
 			
-			debruijn = fdgb.GraphData(reads, k1, verbose=False)
+			debruijn = fdgb.GraphData(reads, k, verbose=False)
 			# delete reads and kmers to save ram:
 			reads = []
 			debruijn.reads = []
@@ -74,30 +135,28 @@ def reconstruct_parts(filename_input_base, filename_output_base, number_of_parts
 			gc.collect()
 			debruijn.contract_unique_overlaps(verbose=False)
 			debruijn.remove_parallel_sequences()
-	
-			debruijn.remove_insignificant_sequences(minimal_weight=minweight)
-			debruijn.contract_unique_overlaps(verbose = False)
-			debruijn.remove_short_sequences(length_bound_by_multiple_of_k=minlengthfactor)
-			debruijn.contract_unique_overlaps(verbose = False)
 			
 			debruijn.remove_single_sequence_components()
 			
-			#debruijn.construct_assembly_ordering_labels()
-			#debruijn.reduce_to_single_path_max_weight()
-			#debruijn.contract_unique_overlaps(verbose = False)
+			debruijn.construct_assembly_ordering_labels()
+			debruijn.reduce_to_single_path_max_weight()
+			debruijn.contract_unique_overlaps(verbose = False)
 			
 			debruijn.get_asqg_output(filename = filename_output+".asqg")
 			debruijn.get_csv_output(filename = filename_output+".csv")
 			debruijn.write_sequences_to_file(filename = filename_output+"_seqsonly.txt", addweights=True)
+			files_of_parts.append(filename_output+"_seqsonly.txt")
+	return files_of_parts
 
-def reconstruct_merge(filename_output_base, number_of_parts, k1, k2):
+def reconstruct_merge(filename_output_base, files_to_merge, merge_k, number_of_parts):
 	reads = []
-	for i in range(number_of_parts):
-		seqfilename = filename_output_base+"_k"+str(k1)+"_p"+str(i)+"_seqsonly.txt"
-		if os.path.isfile(seqfilename):
-			reads += dio.get_reads_from_file(filename = seqfilename)
+	for file in files_to_merge:
+		if os.path.isfile(file):
+			reads += dio.get_reads_from_file(filename = file)
+		else:
+			print "Error! File doesent exist: " + file
 	
-	debruijn = fdgb.GraphData(reads, k2)
+	debruijn = fdgb.GraphData(reads, merge_k)
 	# delete reads and kmers to save ram:
 	debruijn.reads = []
 	debruijn.kmers = []
@@ -106,16 +165,16 @@ def reconstruct_merge(filename_output_base, number_of_parts, k1, k2):
 	
 	debruijn.contract_unique_overlaps(verbose = False)
 	debruijn.remove_parallel_sequences(verbose = False)
-	#debruijn.get_asqg_output(filename = read_dir+"/"+read_basename+"_k"+str(k)+"_merged_step1.asqg")
-	#debruijn.get_csv_output(filename = read_dir+"/"+read_basename+"_k"+str(k)+"_merged_step1.csv")
-	#debruijn.remove_tips()
+	
 	#debruijn.construct_assembly_ordering_labels()
 	#debruijn.reduce_to_single_path_max_weight()
 	#debruijn.contract_unique_overlaps(verbose = False)
 	
-	debruijn.get_asqg_output(filename = filename_output_base+"_k"+str(k1)+"_p"+str(number_of_parts)+"_merged_k"+str(k2)+".asqg")
-	debruijn.get_csv_output(filename = filename_output_base+"_k"+str(k1)+"_p"+str(number_of_parts)+"_merged_k"+str(k2)+".csv")
-	
+	filename_output = filename_output_base+"_p"+str(number_of_parts)+"_merged_k"+str(merge_k)
+	debruijn.get_asqg_output(filename = filename_output+".asqg")
+	debruijn.get_csv_output(filename = filename_output+".csv")
+	debruijn.write_sequences_to_file(filename = filename_output+"_seqsonly.txt", addweights=True)
+	return filename_output+"_seqsonly.txt"
 	
 if __name__ == '__main__':
 
@@ -126,23 +185,24 @@ if __name__ == '__main__':
 	if not os.path.exists(data_dir):
 		os.makedirs(data_dir)
 	# mininum weight of sequences in original graph that are considered:
-	w = 5
+	w = 10
 	# number of parts for partitioning:
 	p = 1000
 	# number of overlapping parts (local redundancy: larger for better reconstruction of total graph from parts):
-	o = 10
+	o = 5
 	# k for debruijn-graphs of parts:
 	k1 = 20
 	# k for reconstruction, should be smaller than k1 to ensure that parts can be merged:
 	k2 = 17
 	# minimum weight of sequences in partition-graphs:
-	w2 = 50
+	w2 = 15
 	# lower bound to the length of sequences in partition-graphs as multiple of k:
 	f = 2
 	
 	# get partitioning of sequences with minimum weight, as defined by param w, p, o:
-	get_sequences_by_params(filename_input=sourcefilename, filename_output=outputfilename, min_weight=w, number_of_parts=p, overlap=o)
+	part_sequence_filenames = get_sequences_by_params(filename_input=sourcefilename, filename_output=outputfilename, min_weight=w, number_of_parts=p, overlap=o)
 	
 	# build debruijn graphs for each part and merge them together:
-	reconstruct_parts(filename_input_base=outputfilename+"_w"+str(w), filename_output_base=outputfilename+"_w"+str(w), number_of_parts=p, k1=k1, minweight=w2, minlengthfactor=f)
-	reconstruct_merge(filename_output_base=outputfilename+"_w"+str(w), number_of_parts=p, k1=k1, k2=k2)
+	parts = reconstruct_parts(part_sequence_filenames, filename_output_base=outputfilename+"_w"+str(w), k=k1, minweight=w2, minlengthfactor=f, allow_recursion=False)
+	if len(parts) > 1:
+		reconstruct_merge(filename_output_base=outputfilename+"_w"+str(w)+"_k"+str(k1), files_to_merge=parts, merge_k=k2, number_of_parts=p)
