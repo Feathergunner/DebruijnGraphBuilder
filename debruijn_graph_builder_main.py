7ea0b533854c30ef08eb3f1802f51535df771bd9
Andreas Goral <angohz@ymail.com>
2017-12-04 20:15:00 +0100

    Changed settings

--- debruijn_graph_builder_main.py
+++ debruijn_graph_builder_main.py
@@ -680,6 +680,6 @@ if __name__ == '__main__':
 	
 	#test_assembly_ordering()
 	#exp_construct_consensus_from_specific_part()
-	#construct_consensus_from_multiple_parts()
+	construct_consensus_from_multiple_parts()
 	#merge_consensus_from_multiple_parts(50, "Data/hcov229e_only.fq", "Output/corona_recons_multiparts/crm_partsize50", 1440, 1446)
-	minimal_test_spectral_partitioning()
+	#minimal_test_spectral_partitioning()
