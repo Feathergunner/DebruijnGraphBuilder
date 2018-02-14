69492e5720c45f0ab7b8f268c0f7a46269ca449e
Andreas Goral <angohz@ymail.com>
2018-02-14 21:16:33 +0100

    Fixed errors, added loading weights to recursive debruijn graphs

--- consensus_constructor.py
+++ consensus_constructor.py
@@ -32,12 +32,12 @@ def construct_consensus_from_multiple_parts(reads, filepath_output, filename_out
 	for part_id in range(number_of_parts):
 		part_sequence = parts[part_id]
 		if len(part_sequence) > 0:
-			part_sequences = [debruijn.sequences[seq.id].sequence for seq in part_sequence]
+			part_sequences = [debruijn.sequences[seq.id].sequence+","+str(debruijn.sequences[seq.id].get_total_weight()) for seq in part_sequence]
 			part_read_ids = debruijn.get_read_of_sequences(part_sequence)
 			part_reads = [reads[i] for i in part_read_ids]
 			#if verbose:
 			print ("Now considering part "+str(part_id)+" which contains "+str(len(part_sequences))+" sequences and "+str(len(part_reads))+" reads.")
-			debruijn_part = fdgb.GraphData([part_sequences], k_part, load_weights=False, remove_tips=True, directed_reads=True, verbose=verbose)
+			debruijn_part = fdgb.GraphData([part_sequences], k_part, load_weights=True, remove_tips=True, directed_reads=True, verbose=verbose)
 			if save_parts:
 				debruijn_part.get_asqg_output(filename = filepath_output+"/"+filename_output_base+"_part"+str(part_id)+".asqg")
 				debruijn_part.get_csv_output(filename = filepath_output+"/"+filename_output_base+"_part"+str(part_id)+".csv")
@@ -69,14 +69,15 @@ def test_with_artificial_data(genome_length, read_length, read_number, error_per
 	if not os.path.exists(filepath_output):
 		os.makedirs(filepath_output)
 	
-	reads = dgen.samplereads(dna, number_of_reads=num_r, replace_error_percentage=er, read_length_mean=r_n, read_length_stddev=0, readlength_distribution='gaussian', inverted_reads=False, verbose=False)
+	dna = dgen.generate_dna(length = genome_length)
+	with open(filepath_output+"/"+name+"_dna.txt", 'w') as outf:
+		outf.write("".join(dna) + '\n')
+		
+	reads = dgen.samplereads(dna, number_of_reads=read_number, replace_error_percentage=error_percentage, read_length_mean=read_length, read_length_stddev=0, readlength_distribution='gaussian', inverted_reads=False, verbose=False)
 	with open(filepath_output+"/"+name+"_reads.txt", 'w') as outf:
 		for r in reads:
 			outf.write(r + '\n')
 			
-	dna = dgen.generate_dna(length = g_n)
-	with open(filepath_output+"/"+name+"_dna.txt", 'w') as outf:
-		outf.write("".join(dna) + '\n')
 	construct_consensus_from_multiple_parts(reads, filepath_output=filepath_output, filename_output_base=name, k_full=k_full, k_part=k_part, number_of_parts=30, overlap=4, save_parts=False, verbose=False)
 
 def test_with_artificial_reads_from_genome(genome_source, genome_name, read_length, read_number, error_percentage, error_type, k_full, k_part, filepath_output):
@@ -105,9 +106,18 @@ def test_with_artificial_reads_from_genome(genome_source, genome_name, read_leng
 			outf.write(r + '\n')
 			
 	construct_consensus_from_multiple_parts(reads, filepath_output=filepath_output, filename_output_base=name, k_full=k_full, k_part=k_part, number_of_parts=30, overlap=4, save_parts=False, verbose=False)
+
+def large_test_replacement_errors():
+	for k1 in [30,35,40]:
+		test_with_artificial_reads_from_genome("Data/human_coronavirus_229e.txt", "hcov229e", 1000, 5000, 15.0, "replace", k1, 19, "Output/test_recursive_recons_corona")
+
+def large_test_indel_errors():
+	for k1 in [30,35,40]:
+		test_with_artificial_reads_from_genome("Data/human_coronavirus_229e.txt", "hcov229e", 1000, 5000, 15.0, "indel", k1, 19, "Output/test_recursive_recons_corona")
+	
+def small_random_test():
+	test_with_artificial_data(1000, 100, 100, 5.0, 30, 19, "test_recursive_recons")
 	
 if __name__ == "__main__":
 	# test:
-	for k1 in [30,35,40]:
-		test_with_artificial_reads_from_genome("Data/human_coronavirus_229e.txt", "hcov229e", 1000, 5000, 15.0, "replace", k1, 15, "Output/test_recursive_recons_corona")
-
+	small_random_test()
