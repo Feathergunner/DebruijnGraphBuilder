2cc53d016f92c6b6d5e0729b510ed5595797ab05
Andreas Goral <angohz@ymail.com>
2018-02-14 14:17:13 +0100

    Added functions for testing

--- consensus_constructor.py
+++ consensus_constructor.py
@@ -9,11 +9,11 @@ import data_gen as dgen
 #import meta
 
 #class ConsensusConstructor:
-def construct_consensus_from_multiple_parts(reads, filepath_output, filename_output_base, k_all, k_part, number_of_parts, overlap, save_parts=False, verbose=False):
+def construct_consensus_from_multiple_parts(reads, filepath_output, filename_output_base, k_full, k_part, number_of_parts, overlap, save_parts=False, verbose=False):
 	if not os.path.exists(filepath_output):
 		os.makedirs(filepath_output)
 		
-	debruijn = fdgb.GraphData([reads], k_all, reduce_data=False, load_weights=False, remove_tips=True, directed_reads=True, verbose=verbose)
+	debruijn = fdgb.GraphData([reads], k_full, reduce_data=False, load_weights=False, remove_tips=True, directed_reads=True, verbose=verbose)
 	debruijn.reduce_to_single_largest_component(verbose=verbose)
 	debruijn.construct_assembly_ordering_labels(verbose=verbose)
 	
@@ -64,24 +64,36 @@ def construct_consensus_from_multiple_parts(reads, filepath_output, filename_out
 	debruijn_recons.get_asqg_output(filename = filepath_output+"/"+filename_output_base+"_recons_reduced.asqg")
 	debruijn_recons.get_csv_output(filename = filepath_output+"/"+filename_output_base+"_recons_reduced.csv")
 	
-if __name__ == "__main__":
-	# test:
-	g_n = 5000
-	r_n = 500
-	num_r = 5000
-	er = 15.0
-	
-	name = "n"+str(g_n)+"_r"+str(num_r)+"_l"+str(r_n)+"_e"+str(int(er))
-	
-	dna = dgen.generate_dna(length = g_n)
-	with open("Output/test_recursice_reconstruction/"+name+"_dna.txt", 'w') as outf:
-		outf.write("".join(dna) + '\n')
+def test_with_artificial_data(genome_length, read_length, read_number, error_percentage, k_full, k_part, filepath_output):
+	name = "n"+str(genome_length)+"_r"+str(read_number)+"_l"+str(read_length)+"_e"+str(int(error_percentage))+"_k1"+str(k_full)+"_k2"+str(k_part)
+	if not os.path.exists(filepath_output):
+		os.makedirs(filepath_output)
 	
 	reads = dgen.samplereads(dna, number_of_reads=num_r, replace_error_percentage=er, read_length_mean=r_n, read_length_stddev=0, readlength_distribution='gaussian', inverted_reads=False, verbose=False)
-	with open("Output/test_recursice_reconstruction/"+name+"_reads.txt", 'w') as outf:
+	with open(filepath_output+"/"+name+"_reads.txt", 'w') as outf:
 		for r in reads:
 			outf.write(r + '\n')
-	
-	#reads = dio.get_reads_from_file("Output/test_recursice_reconstruction/reads.txt")
+			
+	dna = dgen.generate_dna(length = g_n)
+	with open(filepath_output+"/"+name+"_dna.txt", 'w') as outf:
+		outf.write("".join(dna) + '\n')
+	construct_consensus_from_multiple_parts(reads, filepath_output=filepath_output, filename_output_base=name, k_full=k_full, k_part=k_part, number_of_parts=30, overlap=4, save_parts=False, verbose=False)
 
-	construct_consensus_from_multiple_parts(reads, filepath_output="Output/test_recursice_reconstruction", filename_output_base=name, k_all=30, k_part=13, number_of_parts=30, overlap=4, save_parts=False, verbose=False)
+def test_with_artificial_reads_from_genome(genome_source, genome_name, read_length, read_number, error_percentage, k_full, k_part, filepath_output):
+	name = str(genome_name)+"_r"+str(read_number)+"_l"+str(read_length)+"_e"+str(int(error_percentage))+"_k1"+str(k_full)+"_k2"+str(k_part)
+	if not os.path.exists(filepath_output):
+		os.makedirs(filepath_output)
+		
+	dna = dio.get_genome_from_file(genome_source)
+	print dna
+	
+	reads = dgen.samplereads(dna, number_of_reads=read_number, replace_error_percentage=error_percentage, read_length_mean=read_length, read_length_stddev=0, readlength_distribution='gaussian', inverted_reads=False, verbose=False)
+	with open(filepath_output+"/"+name+"_reads.txt", 'w') as outf:
+		for r in reads:
+			outf.write(r + '\n')
+			
+	construct_consensus_from_multiple_parts(reads, filepath_output=filepath_output, filename_output_base=name, k_full=k_full, k_part=k_part, number_of_parts=30, overlap=4, save_parts=False, verbose=False)
+	
+if __name__ == "__main__":
+	# test:
+	test_with_artificial_reads_from_genome("Data/human_coronavirus_229e.txt", "hcov229e", 1000, 500, 1.0, 30, 15, "Output/test_recursive_recons_corona")
