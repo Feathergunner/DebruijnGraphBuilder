d739f0e3125111ad9e200d8b78bbc64f5229e343
Andreas Goral <angohz@ymail.com>
2018-03-29 15:37:56 +0200

    Added examples for consensus reconstruction from 3rd gen reads

---
+++ example_iterative_lowcov_removal.py
@@ -0,0 +1,58 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import data_gen as dgen
+import data_io as dio
+import fast_debruijn_graph_builder as fdgb
+
+import os
+import re
+
+def experiment_iterative_low_coverage_removal(outputdir, dna_length=5000, num_reads=1000, readlength=1000, error_percentage=15.0, k=13, saveparts=True):
+	if not os.path.exists(outputdir):
+		os.mkdir(outputdir)
+
+	# generate dna:
+	dna = dgen.generate_dna(dna_length)
+	# write dna to fasta:
+	dio.write_sequences_to_fasta([''.join(dna)], outputdir+"/dna.fasta")
+	# generate reads:
+	reads = dgen.samplereads(dna, num_reads, indel_error_percentage=error_percentage, read_length_mean=readlength)
+	
+	# initilize:
+	ep_string = "".join(re.split(r'\.',str("%2.2f" % error_percentage)))
+	casename = "itlowcovrem"+"_rl"+str(readlength)+"_nr"+str(num_reads)+"_ei"+ep_string+"_k"+str(k)
+	debruijn = fdgb.GraphData([reads], k=k, directed_reads=True, load_weights=False, reduce_data=True, simplify_graph=True, construct_labels=False, remove_tips=True)
+	# basic reduction:
+	debruijn.remove_insignificant_overlaps(2) # <- removes all overlaps with coverage 1
+	debruijn.remove_tips()
+	debruijn.contract_unique_overlaps()
+	debruijn.remove_single_sequence_components()
+	if saveparts:
+		debruijn.get_asqg_output(filename = outputdir+"/"+casename+"_base.asqg")
+		debruijn.get_csv_output(filename = outputdir+"/"+casename+"_base.csv")
+	
+	# reduction step 1: remove low coverage overlaps
+	debruijn.remove_low_evidence_overlaps_until_graph_decomposes()
+	debruijn.reduce_to_single_largest_component()
+	if saveparts:
+		debruijn.get_asqg_output(filename = outputdir+"/"+casename+"_reduced.asqg")
+		debruijn.get_csv_output(filename = outputdir+"/"+casename+"_reduced.csv")
+	
+	# reduction step 2: remove low coverage sequences:
+	debruijn.remove_low_coverage_sequences_until_graph_decomposes()
+	debruijn.reduce_to_single_largest_component()
+	if saveparts:
+		debruijn.get_asqg_output(filename = outputdir+"/"+casename+"_reduced_2.asqg")
+		debruijn.get_csv_output(filename = outputdir+"/"+casename+"_reduced_2.csv")
+	
+	# construct a consensus sequence:
+	debruijn.construct_assembly_ordering_labels()
+	debruijn.reduce_to_single_path_max_weight()
+	debruijn.contract_unique_overlaps()
+	debruijn.get_asqg_output(filename = outputdir+"/"+casename+"_singlepath.asqg")
+	debruijn.get_csv_output(filename = outputdir+"/"+casename+"_singlepath.csv")
+	debruijn.write_sequences_to_file(filename = outputdir+"/"+casename+"_singlepath.fasta", asfasta = True)
+	
+if __name__ == "__main__":
+	experiment_iterative_low_coverage_removal("Output/itlowcovrem")
---
+++ example_recursive_coverage_refining.py
@@ -0,0 +1,122 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import data_gen as dgen
+import data_io as dio
+import fast_debruijn_graph_builder as fdgb
+
+import os
+import re
+
+def experiment_recursive_coverage_refining(outputdir, dna_length=5000, num_reads=1000, readlength=1000, error_percentage=15.0, number_of_parts=50, overlap=10, saveparts=True):
+	if not os.path.exists(outputdir):
+		os.mkdir(outputdir)
+	if not os.path.exists(outputdir+"/parts"):
+		os.mkdir(outputdir+"/parts")
+		
+	k_base = 25
+	k_part = 15
+	k_merge = 17
+
+	# generate dna:
+	dna = dgen.generate_dna(dna_length)
+	# write dna to fasta:
+	dio.write_sequences_to_fasta([''.join(dna)], outputdir+"/dna.fasta")
+	# generate reads:
+	reads = dgen.samplereads(dna, num_reads, indel_error_percentage=error_percentage, read_length_mean=readlength)
+	
+	# initilize:
+	ep_string = "".join(re.split(r'\.',str("%2.2f" % error_percentage)))
+	casename = "reccovref"+"_rl"+str(readlength)+"_nr"+str(num_reads)+"_ei"+ep_string+"_kb"+str(k_base)+"_kp"+str(k_part)+"_km"+str(k_merge)
+	
+	debruijn_master = fdgb.GraphData([reads], k=25, directed_reads=True, load_weights=False, reduce_data=False, simplify_graph=True, construct_labels=False, remove_tips=True)
+	debruijn_master.reduce_to_single_largest_component()
+	debruijn_master.construct_assembly_ordering_labels()
+			
+	debruijn_master.get_asqg_output(filename = outputdir+"/"+casename+"_master.asqg")
+	debruijn_master.get_csv_output(filename = outputdir+"/"+casename+"_master.csv")
+	
+	parts = debruijn_master.get_partition_of_sequences(number_of_parts, overlap=overlap)
+	
+	parts_consensus_sequences = []
+	parts_reduced_sequences = []
+	for part_id in range(number_of_parts):
+		# get sequences with their weights from this subset as reads:
+		part_seqreads = [seq.sequence+","+str(seq.get_total_weight()) for seq in parts[part_id]]
+		
+		if len(part_seqreads) > 0:
+			# construct de Bruijn graph of this subset:
+			debruijn_part = fdgb.GraphData([part_seqreads], k_part, directed_reads=True, load_weights=True, reduce_data=True, simplify_graph=True, construct_labels=False, remove_tips=True)
+								
+			if saveparts:
+				debruijn_part.get_asqg_output(filename = outputdir+"/parts/"+casename+"_part"+str(part_id)+"_base.asqg")
+				debruijn_part.get_csv_output(filename = outputdir+"/parts/"+casename+"_part"+str(part_id)+"_base.csv")
+			
+			# basic reduction:
+			debruijn_part.remove_insignificant_overlaps(2)
+			debruijn_part.remove_tips()
+			debruijn_part.contract_unique_overlaps()
+			debruijn_part.remove_single_sequence_components()
+			
+			# further reduction:
+			debruijn_part.remove_low_evidence_overlaps_until_graph_decomposes()
+			debruijn_part.reduce_to_single_largest_component()
+			debruijn_part.remove_low_coverage_sequences_until_graph_decomposes()
+			debruijn_part.reduce_to_single_largest_component()
+			
+			if saveparts:
+				#save graph:
+				debruijn_part.get_asqg_output(filename = outputdir+"/parts/"+casename+"_part"+str(part_id)+"_reduced.asqg")
+				debruijn_part.get_csv_output(filename = outputdir+"/parts/"+casename+"_part"+str(part_id)+"_reduced.csv")
+				# save sequences:
+				debruijn_part.write_sequences_to_file(filename = outputdir+"/parts/"+casename+"_part"+str(part_id)+"_sequences", addweights=True)
+			
+			# add sequences of this partial de Bruijn graph to set of sequences to merge
+			parts_reduced_sequences += [seq.sequence+","+str(seq.get_total_weight()) for seq in debruijn_part.sequences if seq.is_relevant]
+				
+			# construct a consensus sequence of this de Bruijn graph:
+			debruijn_part.construct_assembly_ordering_labels(verbose=1)
+			debruijn_part.reduce_to_single_path_max_weight()
+			debruijn_part.contract_unique_overlaps()
+			
+			if saveparts:
+				# save graph:
+				debruijn_part.get_asqg_output(filename = outputdir+"/parts/"+casename+"_part"+str(part_id)+"_singlepath.asqg")
+				debruijn_part.get_csv_output(filename = outputdir+"/parts/"+casename+"_part"+str(part_id)+"_singlepath.csv")
+				# save consensus sequence:
+				debruijn_part.write_sequences_to_file(filename = outputdir+"/parts/"+casename+"_part"+str(part_id)+"_singlepath", addweights=True)
+			
+			parts_consensus_sequences += debruijn_part.get_relevant_sequences()
+			
+	# merge once from sequences and once from consensus sequences:
+	readsets = [parts_reduced_sequences, parts_consensus_sequences]
+	readset_names = ["sequences", "consensus"]
+	for i in range(len(readsets)):
+		debruijn_merge_sequences = fdgb.GraphData([readsets[i]], k_merge, directed_reads=True, load_weights=True, reduce_data=True, simplify_graph=True, construct_labels=False, remove_tips=True)
+		debruijn_merge_sequences.remove_insignificant_overlaps(2)
+		debruijn_merge_sequences.remove_tips()
+		debruijn_merge_sequences.contract_unique_overlaps()
+		debruijn_merge_sequences.remove_single_sequence_components()
+		
+		debruijn_merge_sequences.get_asqg_output(filename = data_dir_sub+"/"+casename+"_merge_"+readset_names[i]+"_base.asqg")
+		debruijn_merge_sequences.get_csv_output(filename = data_dir_sub+"/"+casename+"_merge_"+readset_names[i]+"_base.csv")
+		
+		debruijn_merge_sequences.remove_low_evidence_overlaps_until_graph_decomposes()
+		debruijn_merge_sequences.reduce_to_single_largest_component()
+		debruijn_merge_sequences.remove_low_coverage_sequences_until_graph_decomposes()
+		debruijn_merge_sequences.reduce_to_single_largest_component()
+		
+		debruijn_merge_sequences.get_asqg_output(filename = data_dir_sub+"/"+casename+"_merge_"+readset_names[i]+"_reduced.asqg")
+		debruijn_merge_sequences.get_csv_output(filename = data_dir_sub+"/"+casename+"_merge_"+readset_names[i]+"_reduced.csv")
+		
+		debruijn_merge_sequences.construct_assembly_ordering_labels()
+		debruijn_merge_sequences.reduce_to_single_path_max_weight()
+		debruijn_merge_sequences.contract_unique_overlaps()
+		
+		debruijn_merge_sequences.get_asqg_output(filename = data_dir_sub+"/"+casename+"_merge_"+readset_names[i]+"_singlepath.asqg")
+		debruijn_merge_sequences.get_csv_output(filename = data_dir_sub+"/"+casename+"_merge_"+readset_names[i]+"_singlepath.csv")
+		debruijn_merge_sequences.write_sequences_to_file(filename = data_dir_sub+"/"+casename+"_merge_"+readset_names[i]+"_singlepath.fasta", asfasta = True)
+	
+if __name__ == "__main__":
+	experiment_recursive_coverage_refining("Output/reccovref")
+	
