dab012fb92b85eadd8a53fc3c806f41745164411
Andreas Goral <angohz@ymail.com>
2018-04-19 13:41:33 +0200

    Added settings

--- datasets_experiments.py
+++ datasets_experiments.py
@@ -93,6 +93,17 @@ set_cons3g_r_hc_large = {
 	"overlaps" : [5,10,20],
 	"name" : "cons_3g_replace_hc"}
 	
+set_cons3g_i_hc_large = {
+	"kmer_lengths" : [13,15,17,19,21,23,25,29,33,37,41],
+	"readlengths" : [1000],
+    "numbers_of_reads" : [500,750,1000,1500],
+	"error_type" : "indel",
+	"error_rates" : [15.0],
+	"k_base" : 25,
+	"number_of_parts" : [10,25,50],
+	"overlaps" : [5,10,20],
+	"name" : "cons_3g_indel_hc"}
+	
 set_cons3g_r_hc_detail = {
 	"kmer_lengths" : [13,14,15,16,17,18,19],
 	"readlengths" : [1000],
