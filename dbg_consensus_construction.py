ee3ae0116f12fec805d620923542b86994e3210d
Andreas Goral <angohz@ymail.com>
2018-04-20 10:37:29 +0200

    Simplified covref-algorithm

--- dbg_consensus_construction.py
+++ dbg_consensus_construction.py
@@ -46,12 +46,13 @@ def simplecons(	reads,
 # low coverage feature removal:
 def cons_locofere(	reads,
 					k,
-					name		= "",
-					outputdir	= ".",
-					saveparts 	= True,
-					saveresult	= True,
-					verbose		= False):
-	debruijn = fdgb.GraphData(reads, k=k, directed_reads=True, load_weights=False, reduce_data=True, simplify_graph=True, construct_labels=False, remove_tips=False)
+					name			= "",
+					outputdir		= ".",
+					weightedreads	= False,
+					saveparts 		= True,
+					saveresult		= True,
+					verbose			= False):
+	debruijn = fdgb.GraphData(reads, k=k, directed_reads=True, load_weights=weightedreads, reduce_data=True, simplify_graph=True, construct_labels=False, remove_tips=False)
 	
 	if saveparts:
 		debruijn.get_asqg_output(filename = outputdir+"/"+name+"_0_unsimplified.asqg")
@@ -121,73 +122,10 @@ def cons_covref(reads,
 		part_seqreads = [seq.sequence+","+str(seq.get_total_weight()) for seq in parts[part_id]]
 		
 		if len(part_seqreads) > 0:
-			# construct de Bruijn graph of this subset:
-			debruijn_part = fdgb.GraphData([part_seqreads], k_part, directed_reads=True, load_weights=True, reduce_data=True, simplify_graph=True, construct_labels=False, remove_tips=True)
-								
-			if saveparts:
-				debruijn_part.get_asqg_output(filename = outputdir+"/parts/"+name+"_part"+str(part_id)+"_base.asqg")
-				debruijn_part.get_csv_output(filename = outputdir+"/parts/"+name+"_part"+str(part_id)+"_base.csv")
+			# construct consensus of this subset:			
+			parts_consensus_sequences.append(cons_locofere(part_seqreads, k_part, name = name+"_part"+str(part_id), outputdir = outputdir+"/parts", weightedreads=True, saveparts = True, saveresult = True, verbose = False))
 			
-			# basic reduction:
-			debruijn_part.remove_tips()
-			debruijn_part.remove_single_sequence_components()
-			
-			# further reduction:
-			debruijn_part.remove_low_evidence_overlaps_until_graph_decomposes()
-			debruijn_part.reduce_to_single_largest_component()
-			debruijn_part.remove_low_coverage_sequences_until_graph_decomposes()
-			debruijn_part.reduce_to_single_largest_component()
-			
-			if saveparts:
-				#save graph:
-				debruijn_part.get_asqg_output(filename = outputdir+"/parts/"+name+"_part"+str(part_id)+"_reduced.asqg")
-				debruijn_part.get_csv_output(filename = outputdir+"/parts/"+name+"_part"+str(part_id)+"_reduced.csv")
-				# save sequences:
-				debruijn_part.write_sequences_to_file(filename = outputdir+"/parts/"+name+"_part"+str(part_id)+"_sequences", addweights=True)
-			
-			# add sequences of this partial de Bruijn graph to set of sequences to merge
-			parts_reduced_sequences += [seq.sequence+","+str(seq.get_total_weight()) for seq in debruijn_part.sequences if seq.is_relevant]
-				
-			# construct a consensus sequence of this de Bruijn graph:
-			debruijn_part.construct_assembly_ordering_labels()
-			debruijn_part.reduce_to_single_path_max_weight()
-			debruijn_part.contract_unique_overlaps()
-			
-			if saveparts:
-				# save graph:
-				debruijn_part.get_asqg_output(filename = outputdir+"/parts/"+name+"_part"+str(part_id)+"_singlepath.asqg")
-				debruijn_part.get_csv_output(filename = outputdir+"/parts/"+name+"_part"+str(part_id)+"_singlepath.csv")
-				# save consensus sequence:
-				debruijn_part.write_sequences_to_file(filename = outputdir+"/parts/"+name+"_part"+str(part_id)+"_singlepath", addweights=True)
-			
-			parts_consensus_sequences += debruijn_part.get_relevant_sequences()
-			
-	# merge once from sequences and once from consensus sequences:
-	#readsets = [parts_reduced_sequences, parts_consensus_sequences]
-	#readset_names = ["sequences", "consensus"]
-	#for i in range(len(readsets)):
+	# merge once from consensus sequences:
 	for km in k_merge:
-		#casename_merge = name+"_merge_"+readset_names[i]+"_km"+str(km)
-		debruijn_merge_sequences = fdgb.GraphData(parts_consensus_sequences, km, directed_reads=True, load_weights=True, reduce_data=True, simplify_graph=True, construct_labels=False, remove_tips=True)
-		debruijn_merge_sequences.remove_tips()
-		debruijn_merge_sequences.contract_unique_overlaps()
-		debruijn_merge_sequences.remove_single_sequence_components()
-		
-		debruijn_merge_sequences.get_asqg_output(filename = outputdir+"/"+name+"_km"+str(km)+"_1_base.asqg")
-		debruijn_merge_sequences.get_csv_output(filename = outputdir+"/"+name+"_km"+str(km)+"_1_base.csv")
-		
-		debruijn_merge_sequences.remove_low_evidence_overlaps_until_graph_decomposes()
-		debruijn_merge_sequences.reduce_to_single_largest_component()
-		debruijn_merge_sequences.remove_low_coverage_sequences_until_graph_decomposes()
-		debruijn_merge_sequences.reduce_to_single_largest_component()
-		
-		debruijn_merge_sequences.get_asqg_output(filename = outputdir+"/"+name+"_km"+str(km)+"_2_reduced.asqg")
-		debruijn_merge_sequences.get_csv_output(filename = outputdir+"/"+name+"_km"+str(km)+"_2_reduced.csv")
-		
-		debruijn_merge_sequences.construct_assembly_ordering_labels()
-		debruijn_merge_sequences.reduce_to_single_path_max_weight()
-		debruijn_merge_sequences.contract_unique_overlaps()
-		
-		debruijn_merge_sequences.get_asqg_output(filename = outputdir+"/"+name+"_km"+str(km)+"_3_singlepath.asqg")
-		debruijn_merge_sequences.get_csv_output(filename = outputdir+"/"+name+"_km"+str(km)+"_3_singlepath.csv")
-		debruijn_merge_sequences.write_sequences_to_file(filename = outputdir+"/"+name+"_km"+str(km)+"_3_singlepath.fasta", asfasta = True)
+		casename_merge = name+"_km"+str(km)
+		cons_locofere(parts_consensus_sequences, km, name=casename_merge, outputdir=outputdir, weightedreads=True, saveparts=True, saveresult=True, verbose=False)
