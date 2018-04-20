e902713d62a22cf03accdc49b99d04eff726529b
Andreas Goral <angohz@ymail.com>
2018-04-20 10:36:28 +0200

    Added check for existing files

--- apply_blast.py
+++ apply_blast.py
@@ -10,20 +10,21 @@ path_to_blast_binary = "../blast/bin"
 def init_blast_db(dna_fasta_filename):
 	cmd = subprocess.Popen([path_to_blast_binary+"\makeblastdb", "-dbtype", "nucl", "-in", dna_fasta_filename])
 	
-def compute_blast_results(data_dir_sub, dna_fasta_filename):
+def compute_blast_results(data_dir_sub, dna_fasta_filename, new_data=False):
 	for file in os.listdir(data_dir_sub):
 		file_name_parts = re.split(r'\.',file)
 		if file_name_parts[-1] == "fasta":
 			filename = ".".join(file_name_parts[:-1])
 			if not filename == "dna":
-				print ("Consider file "+filename)
-				querystring = data_dir_sub+"/"+file
-				outputfile = data_dir_sub+"\\"+filename+"_blastresults.out"
-				fullcommand = path_to_blast_binary+"\\blastn -query " +querystring+ " -db " + dna_fasta_filename + " -out " + outputfile
-				fullcommand = fullcommand.replace("/","\\")
-				commandlist = fullcommand.split(' ')
-				#print fullcommand
-				subprocess.call(commandlist, cwd=".\\")
+				if not os.path.isfile(data_dir_sub+"/"+filename+"_blastresults.out") or new_data:
+					print ("Consider file "+filename)
+					querystring = data_dir_sub+"/"+file
+					outputfile = data_dir_sub+"\\"+filename+"_blastresults.out"
+					fullcommand = path_to_blast_binary+"\\blastn -query " +querystring+ " -db " + dna_fasta_filename + " -out " + outputfile
+					fullcommand = fullcommand.replace("/","\\")
+					commandlist = fullcommand.split(' ')
+					#print fullcommand
+					subprocess.call(commandlist, cwd=".\\")
 
 if __name__ == "__main__":
 	data_dir_base = "Output"
